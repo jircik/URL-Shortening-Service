ebc908a32f6b3cccf26f51306e85727c242e63ec
jircik <oarthurjircik@gmail.com>
2026-03-21 14:16:29 -0300

    feat: add swagger documentation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,23 @@
 import express from 'express';
 import shortUrls from "./shortUrlRoutes.js";
 
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     summary: Health check
+ *     description: Returns a welcome message to confirm the API is running.
+ *     tags:
+ *       - Health
+ *     responses:
+ *       200:
+ *         description: API is up and running
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *               example: Welcome
+ */
 const routes = (app) => {
     app.route("/").get((req, res) => res.status(200).send("Welcome"));
 
