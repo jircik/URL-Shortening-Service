3eb20cc0322201bf507f9bceb0d3b8faa8206d48
jircik <oarthurjircik@gmail.com>
2026-03-30 14:55:17 -0300

    chore: switch to @opennextjs/cloudflare adapter for Cloudflare Pages deployment

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,8 +3,6 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { AuthProvider } from '@/hooks/AuthProvider';
 
-export const runtime = 'edge';
-
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
